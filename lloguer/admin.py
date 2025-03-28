f3ee70c98d4df16def4f9ce3a7034e91656486dc
WilliamSCastro <william.sargisson.castro@gmail.com>
2025-03-28 15:54:09 +0100

    fita 3

--- lloguer/admin.py
+++ lloguer/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Automobil)
+admin.site.register(Reserva)
 # Register your models here.
--- lloguer/models.py
+++ lloguer/models.py
@@ -8,3 +8,15 @@ class Automobil(models.Model):
     matricula = models.CharField(max_length=10)
     def __str__(self):
         return f"{self.marca} {self.model} {self.matricula}"
+
+class Reserva(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Ensures each vote is linked to a user
+    cotxe = models.ForeignKey(Automobil, on_delete=models.CASCADE, related_name="vots")
+    data_inici = models.DateField()
+    data_final = models.DateField(blank=True,null=True)
+
+    class Meta:
+        unique_together = ('cotxe', 'data_inici')  # Ensures a user can only vote once per option
+
+    def __str__(self):
+        return f"[{self.data_inici}] {self.user.username} → {self.cotxe.marca} {self.cotxe.model} {self.cotxe.matricula}"
